67f8274e4ed6b2f91f67ea7db8ba11ea47cebcea
Nidala96 <pittaufabio2@gmail.com>
2024-01-24 22:51:53 +0100

    Aggiunta variabile di ambiente per l'url del front-end

--- src/main/java/it/fabio/libreria/configuration/CorsConfig.java
+++ src/main/java/it/fabio/libreria/configuration/CorsConfig.java
@@ -7,10 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private final String frontEndUrl = System.getenv("FRONT_END_URL");
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/libro/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontEndUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
